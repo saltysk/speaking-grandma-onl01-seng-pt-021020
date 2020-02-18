d94f89f3bab7cf4822ab4e9c7c112ad5ce1114d9
Leo Kim <sotysk@gmail.com>
2020-02-18 21:22:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase == "Hi Nana, how are you?"
+    'HUH?! SPEAK UP, SONNY!'
   else
 end
 end
