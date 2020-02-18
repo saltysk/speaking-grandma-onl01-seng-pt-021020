d4a15d507d919358b0860a68e1693a624c47e9c4
Leo Kim <sotysk@gmail.com>
2020-02-18 21:29:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(phrase)
     'HUH?! SPEAK UP, SONNY!'
   elsif phrase == "Hi!"
     'HUH?! SPEAK UP, SONNY!'
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    'NO, NOT SINCE 1938'
   else
 end
 end
